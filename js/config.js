b9de51217b70b05badd5c7d112111816f6bf05e7
Daniyar <yeskaliyevdan@hotmail.com>
2022-11-26 14:21:26 -0800

    removed aws cognito and api keys

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_l7kVZxxuq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5vb2foiaa7iaj1mahpu0l0ljtu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://5e95bckwcd.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
